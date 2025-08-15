34f76cf8657411278f5528c871ea2463e4d75195
Narendra657 <narendrareddybuthukuri04@gmail.com>
2025-08-15 11:42:27 +0530

    Add files via upload

---
+++ basiccode day-1.py
@@ -0,0 +1,45 @@
+#reverse a given string without using bulit in reverse functions
+def reverse_string(s):
+    if len(s) == 0:
+        return s
+    return reverse_string(s[1:]) + s[0]
+
+s = input("enter name: ")
+print(reverse_string(s))
+
+
+#plaindrome or not
+s = input("enter the plaindrome: ")
+is_palindrome = True
+for i in range(len(s) // 2):
+    if s[i] != s[len(s) - i - 1]:
+        is_palindrome = False
+        break
+
+if is_palindrome:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+#vowels and consonants
+text = input("Enter a string: ").lower()
+vowels = sum(1 for ch in text if ch in "aeiou")
+consonants = sum(1 for ch in text if ch.isalpha() and ch not in "aeiou")
+print("Vowels:", vowels, "Consonants:", consonants)
+
+# text replacing
+text = input("Enter a string: ")
+text = text.replace(" ", "")
+print(text)
+
+
+#frequency
+text = input("Enter a string: ")
+freq = {}
+for ch in text:
+    freq[ch] = freq.get(ch, 0) + 1
+print(freq)
+
+
+
+
